d937f96b4d1c3587dd9d85d4fbd483ef588e6efb
poseidon0z <adityaprabhu1290@gmail.com>
2024-04-16 12:42:23 +0530

    Finished frontend

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { HashRouter, Route, Routes } from 'react-router-dom';
 import Home from './components/home';
 import Nav from './components/Nav';
 import Recruiter from './components/Recruiter';
+import UserStuff from './components/UserStuff';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/joinus" element={<InputFileUpload />} />
           <Route path="/recruiter" element={<Recruiter />} />
+          <Route path="/user" element={<UserStuff />} />
         </Routes>
       </HashRouter>
     </>
